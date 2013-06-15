51d3c3b7ced1917fc6df2d4bc966814b38ab0e4e
EFulmer <EFulmer@gmail.com>
2013-06-15 18:41:51 -0400

    additional solution for problem 8

--- src/problem-08.clj
+++ src/problem-08.clj
@@ -1,11 +1,17 @@
 ; Problem 08: Eliminate consecutive duplicates of list elements.
 
-; Recursion with loop:
+; Direct recursion:
 (defn my-compress [xs]
+  (let [first-repeats (fn [x] (= x (first xs)))
+        front (take-while first-repeats xs)
+        back (drop-while first-repeats xs)]
+    (if (empty? back) 
+      (list front) 
+      (cons front (my-compress back)))))
+
+; Recursion with loop:
+(defn my-compress-2 [xs]
   (loop [acc [(first xs)] cca (rest xs)]
     (cond (empty? cca) acc
           (= (first cca) (last acc)) (recur acc (rest cca))
-          true (recur (conj acc (first cca)) (rest cca))
-          )))
-
-;
+          true (recur (conj acc (first cca)) (rest cca)))))
--- src/problem-09.clj
+++ src/problem-09.clj
@@ -1,7 +1,12 @@
 ; Problem 09: Pack consecutive duplicate list elements into sublists.
 
+; Direct recursion:
+(defn pack [xs]
+  (cond (empty? xs) xs
+        true (conj (pack (drop-while (fn [x] (= x (first xs))) xs)) (take-while (fn [x] (= x (first xs))) xs))))
+
 ; Recursion using loop-recur:
-(defn pack[xs]
+(defn pack-2 [xs]
   (loop [acc [] xs xs]
     (cond (empty? xs) acc
           true (recur (conj acc (take-while (fn [x] (= x (first xs))) xs))

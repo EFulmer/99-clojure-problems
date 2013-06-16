cda06c658a03ae8ddfbdf91e427a58565d8b58d6
EFulmer <EFulmer@gmail.com>
2013-06-16 16:26:44 -0400

    remove problem12.clj

--- src/problem12.clj
+++
@@ -1,6 +0,0 @@
-; Problem 12: Decode a run-length encoded list.
-
-; Direct recursion with destructuring (hooray!)
-(defn my-decode ([[num elem] & more] (cons (repeat num elem) (my-decode more)))
-  ([xs] (if (empty? xs) xs (cons (first xs) (my-decode (rest xs))))))
-

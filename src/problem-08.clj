4432081b4c4c49a1f7ce0e46eb31e13934b58c1e
EFulmer <EFulmer@gmail.com>
2013-06-15 20:29:53 -0400

    whooooops

--- src/problem-08.clj
+++ src/problem-08.clj
@@ -1,16 +1,7 @@
 ; Problem 08: Eliminate consecutive duplicates of list elements.
 
-; Direct recursion:
-(defn my-compress [xs]
-  (let [first-repeats (fn [x] (= x (first xs)))
-        front (take-while first-repeats xs)
-        back (drop-while first-repeats xs)]
-    (if (empty? back) 
-      (list front) 
-      (cons front (my-compress back)))))
-
 ; Recursion with loop:
-(defn my-compress-2 [xs]
+(defn my-compress [xs]
   (loop [acc [(first xs)] cca (rest xs)]
     (cond (empty? cca) acc
           (= (first cca) (last acc)) (recur acc (rest cca))

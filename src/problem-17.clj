e69c285fdb46de1260b78e5cf070cd82a3b6a18d
EFulmer <EFulmer@gmail.com>
2013-06-24 20:30:42 -0400

    improved problem 17

--- src/problem-17.clj
+++ src/problem-17.clj
@@ -1,6 +1,13 @@
 ; Problem 17: Split a list into two parts; the length of the first part is given.
 
-(defn split [coll n]
+(defn split 
+  "Returns a pair of sequences: one containing the first n elements of coll and the other containing the remaining elements of coll."
+  [coll n]
   (loop [a '() z coll x n]
     (if (= 0 x) (concat (list (reverse a)) (list z))
         (recur (cons (first z) a) (rest z) (dec x)))))
+
+(defn split-2
+    "Returns a pair of sequences: one containing the first n elements of coll and the other containing the remaining elements of coll."
+    [coll n]
+    (cons (take n coll) (list (drop n coll))))
